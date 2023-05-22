9384712e8d42314d4f4b14106291f34fa3c66531
Fedor <49358374+HiBlakk@users.noreply.github.com>
2023-05-22 12:11:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -19,11 +19,10 @@ btn1.addEventListener("click", function () {
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбпали товар 1!");
+		tg.MainButton.setText("Вы выбрали товар 1!");
 		item = "1";
 		tg.MainButton.show();
 	}
-	// body...
 });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
