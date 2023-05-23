d9f7830ecfe5e5ea961ff9b5916c4866fbf5eb5e
Fedor <49358374+HiBlakk@users.noreply.github.com>
2023-05-23 19:41:14 +0300

    Update tags.js

--- tags.js
+++ tags.js
@@ -9,22 +9,33 @@ filterButtons.forEach(button => {
   button.addEventListener('click', () => {
     const tag = button.dataset.tag; // Получаем значение тега из data-атрибута кнопки
 
+    // Проверяем, активна ли уже кнопка
+    const isActive = button.classList.contains('active');
+
     // Удаляем класс 'active' у всех кнопок
     filterButtons.forEach(btn => {
       btn.classList.remove('active');
     });
 
-    // Добавляем класс 'active' к нажатой кнопке
-    button.classList.add('active');
-
-    // Показываем только блоки item с соответствующим тегом или скрываем все блоки item при отсутствии тега
-    items.forEach(item => {
-      const itemTags = item.dataset.tags; // Получаем значение тегов из data-атрибута блока item
-      if (itemTags.includes(tag)) {
+    // Показываем все товары при повторном нажатии на активный тег
+    if (isActive) {
+      items.forEach(item => {
         item.style.display = 'block'; // Отображаем блок item
-      } else {
+      });
+    } else {
+      // Добавляем класс 'active' к нажатой кнопке
+      button.classList.add('active');
+
+      // Скрываем все блоки item
+      items.forEach(item => {
         item.style.display = 'none'; // Скрываем блок item
-      }
-    });
+      });
+
+      // Показываем только блоки item с соответствующим тегом
+      const filteredItems = document.querySelectorAll(`.item[data-tags*="${tag}"]`);
+      filteredItems.forEach(item => {
+        item.style.display = 'block'; // Отображаем блок item
+      });
+    }
   });
 });
