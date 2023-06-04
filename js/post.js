f60adbe09671514c7643feebd5d5108ac7eb8945
HiBlakk <hiblakk@gmail.com>
2023-06-04 22:13:16 +0300

    пробуем всосать

---
+++ js/post.js
@@ -0,0 +1,21 @@
+document.querySelector('#checkout-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Предотвращает перезагрузку страницы
+
+  // Выполняем POST-запрос с данными формы
+  fetch('https://hiblakk.github.io', {
+    method: 'POST',
+    body: new FormData(event.target), // event.target ссылается на форму
+  })
+  .then(function(response) {
+    // Проверяем, что запрос успешно выполнен
+    if (response.ok) {
+      // Перенаправляем пользователя на страницу success.html
+      window.location = 'success.html';
+    } else {
+      throw new Error('Request failed: ' + response.status);
+    }
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
+});
