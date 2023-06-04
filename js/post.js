9948550eb6a1e02aa912f92e1912f0badc06b095
HiBlakk <hiblakk@gmail.com>
2023-06-04 22:23:35 +0300

    Update post.js

--- js/post.js
+++ js/post.js
@@ -1,21 +1,29 @@
-document.querySelector('#checkout-form').addEventListener('submit', function(event) {
-  event.preventDefault(); // Предотвращает перезагрузку страницы
+window.addEventListener('DOMContentLoaded', function() {
+  const checkoutForm = document.querySelector('#checkout-form');
 
-  // Выполняем POST-запрос с данными формы
-  fetch('https://hiblakk.github.io/cart.html', {
-    method: 'POST',
-    body: new FormData(event.target), // event.target ссылается на форму
-  })
-  .then(function(response) {
-    // Проверяем, что запрос успешно выполнен
-    if (response.ok) {
-      // Перенаправляем пользователя на страницу success.html
-      window.location = 'success.html';
-    } else {
-      throw new Error('Request failed: ' + response.status);
-    }
-  })
-  .catch(function(error) {
-    console.error(error);
+  checkoutForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let items = JSON.parse(localStorage.getItem('items') || '[]');
+
+    let form_data = new FormData(checkoutForm);
+
+    // Предполагая, что на сервере ожидается параметр 'items' в виде JSON-строки
+    form_data.append('items', JSON.stringify(items));
+
+    fetch('https://hiblakk.github.io/submit', {
+      method: 'POST',
+      body: form_data
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      // Если сервер успешно принял данные, перенаправляем на страницу 'success.html'
+      window.location.href = 'https://hiblakk.github.io/success.html';
+    })
+    .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+    });
   });
 });
