e00bb5bb1b6f49e7d469e007145d57e565f2d15c
HiBlakk <bronzakatana@gmail.com>
2023-10-21 16:59:42 +0300

    🔧 chore(cart.html): add promo.js script to cart.html to enable promo code functionality 🔧 chore(style.css): remove unused styles from style.css to improve code readability and maintainability 🔧 chore(index.html): update item names in index.html to match the actual products being displayed ✨ feat(promo.js): add promo code functionality to calculate the total price of items in the cart

---
+++ js/promo.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const applyPromoButton = document.getElementById('applyPromoButton');
+
+    applyPromoButton.addEventListener('click', function () {
+        // Получение значения промокода из поля ввода
+        const promoInput = document.getElementById('promo');
+        const promocode = promoInput.value;
+
+        // Пример отправки POST-запроса
+        const url = 'https://212.109.194.133:8443/shop/calculate-cart-total/';
+        const requestData = {
+            items: [
+                { bouquete: 'Васаби', amount: 2 },
+                { bouquete: 'Вуаяж', amount: 1 },
+            ],
+            tgid: '463863956',
+            promocode: promocode, 
+        };
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestData),
+        })
+        .then(response => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                throw new Error(`Ошибка: ${response.status}`);
+            }
+        })
+        .then(result => {
+            const totalPrice = result.total_price;
+            console.log(`Общая стоимость: ${totalPrice}`);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    });
+});
